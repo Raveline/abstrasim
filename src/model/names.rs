aa1c0975033b44a00b39da73a5e111a9534f8aa2
Raveline <eraveline@gmail.com>
2016-05-07 16:56:40 +0200

    Testing examples of random name generation

--- src/model/names.rs
+++ src/model/names.rs
@@ -1,5 +1,8 @@
+extern crate rand;
+
 use std::vec;
 use std::collections::BTreeMap;
+use model::names::rand::Rng;
 
 #[derive(Serialize, Deserialize)]
 pub struct SectorWords {
@@ -16,4 +19,21 @@ pub struct SectorNames {
 pub struct NameList {
     pub sectors: BTreeMap<String, SectorNames>,
     pub names: Vec<String>
+}
+
+fn pick_random(from: &Vec<String>) -> &String {
+    rand::thread_rng().choose(from).expect("Tried to pick random from an empty list.")
+}
+
+fn random_simple_name(from: &NameList) -> &String {
+    pick_random(&from.names)
+}
+
+pub fn random_language_generic_first(sector: &str, language: &str, from: &NameList) -> String {
+    let sector_langs = from.sectors.get(sector).expect("No such sector");
+    let lang_last = sector_langs.words.get(language).expect("No such language");
+    let generic_langs = from.sectors.get("generic").expect("No such sector");
+    let lang_fst = generic_langs.words.get(language).expect("No such language");
+    let n = format!("{} {}", pick_random(&lang_fst.first), pick_random(&lang_last.last));
+    n.to_string()
 }

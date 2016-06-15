72b0364831d47c4d27a0e75bf9aeb1ab4f68482f
Raveline <eraveline@gmail.com>
2016-06-15 11:54:39 +0200

    Display implementation for Sectors

--- src/model/sectors.rs
+++ src/model/sectors.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug, PartialEq, Eq, Ord, PartialOrd, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
 pub enum Sector {
@@ -17,4 +19,25 @@ pub enum Sector {
     Banking,
     Distribution,
     Insurance
+}
+
+impl fmt::Display for Sector {
+    fn fmt(&self, f:&mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", match *self {
+            Sector::Generic => "",
+            Sector::OilEnergy => "Oil & Energy",
+            Sector::Software => "Software",
+            Sector::Mining => "Mining",
+            Sector::Logistics => "Logistics",
+            Sector::Pharmaceutics => "Pharmaceutics",
+            Sector::Tourism => "Tourism",
+            Sector::Luxury => "Luxury",
+            Sector::Automobile => "Automobile",
+            Sector::Food => "Food",
+            Sector::Media => "Media",
+            Sector::Telecom => "Telecom",
+            Sector::Banking => "Banking",
+            Sector::Distribution => "Distribution",
+            Sector::Insurance => "Insurance" })
+    }
 }

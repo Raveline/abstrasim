ec6ff842568e4179fe9b41fe542c4d53896fdd2f
Raveline <eraveline@gmail.com>
2016-06-16 17:27:58 +0200

    Add stock register to World struct

--- src/model/world.rs
+++ src/model/world.rs
@@ -1,21 +1,29 @@
+use std::collections::BTreeMap;
 use rand::thread_rng;
 use rand::Rng;
+
+use model;
+use init;
+use model::names::NameList;
 use model::business::Business;
 use model::sectors::Sector;
-use std::collections::BTreeMap;
+use model::stocks::Stocks;
+use init::names;
 
 pub type SectorBusinesses = BTreeMap<Sector, Vec<Business>>;
 
-pub struct World {
-    pub sectors: SectorBusinesses
+pub struct World<'a> {
+    pub sectors: SectorBusinesses,
+    pub stocks: Stocks<'a>
 }
 
-impl World {
-    pub fn new() -> World {
-        World { sectors : BTreeMap::new() }
+impl <'a> World<'a> {
+    pub fn new() -> World<'a> {
+        World { sectors: BTreeMap::new(),
+                stocks: Stocks::new() }
     }
 
-    pub fn tick(&mut self) {
+    pub fn tick(&'a mut self) {
         for (_, biz) in self.sectors.iter_mut() {
             let sectorial_health = 0.5;
             let random_factor = thread_rng().gen_range(0., 1.);
@@ -27,7 +35,27 @@ impl World {
                 let change = b.compute_capitalisation_change(random_factor);
                 b.capitalisation = (b.capitalisation as f32 + change) as u32;
                 b.perception = b.compute_perception(random_factor);
+                self.stocks.push(&b, b.get_current_stock_value());
             }
         }
+    }
+
+    pub fn generate_world() -> World<'a> {
+        let nl = init::names::build_name_list();
+        let mut w = World::new();
+        for sec in Sector::iterate() {
+            w.sectors.insert(*sec, World::generate_sector(&nl, *sec));
+        }
+        w
+    }
+
+    fn generate_sector(nl: &NameList, sector : Sector) -> Vec<Business> {
+        let mut businesses = vec!();
+        for _ in 0..10 {
+            let name = model::names::random_company_name(sector, "english", &nl);
+            let b = Business::new(name, sector);
+            businesses.push(b);
+        }
+        businesses
     }
 }
